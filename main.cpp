8a05ecf8e1c8cd80cffdb97e178c8aecbeb9e9b3
임기정 <gijungdduk@naver.com>
2024-02-26 20:14:07 +0900

    add `static void press_any_key(void)`

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include "scratch.hpp"
 
+static void press_any_key(void);
+
 int main(void)
 {
     test::nonogramsolverv2();
+    press_any_key();
     return 0;
+}
+
+void press_any_key()
+{
+    std::cin.get();
 }
